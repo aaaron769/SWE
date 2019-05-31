ed40151c2f3eaf353647e6a73b8dfcf3d32f8293
tungnd1707 <pooh.1707.07@gmail.com>
2019-05-31 11:28:41 -0500

    Add Directory

---
+++ src/test/java/com/swe/project/QuaroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.swe.project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuaroApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
